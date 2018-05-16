f3006e073adcef400c61ba66c74c77481b627b84
gihpuccini <gxp6079@g.rit.edu>
2018-05-15 22:47:30 -0400

    In progress

--- src/Controle/Player.java
+++ src/Controle/Player.java
@@ -1,4 +1,12 @@
 package Controle;
 
+import java.util.ArrayList;
+
 public class Player {
+
+    private DiceTable dices;
+
+    public DiceTable getDices() {
+        return dices;
+    }
 }
--- src/Controle/ScoreTable.java
+++ src/Controle/ScoreTable.java
@@ -1,4 +1,41 @@
 package Controle;
 
+import java.util.HashMap;
+
 public class ScoreTable {
+
+    Player player;
+    HashMap<String, Integer> scores;
+
+    public ScoreTable(Player player){
+        this.player = player;
+        this.scores = new HashMap<>();
+        scores.put("Ones", 0);
+        scores.put("Two", 0);
+        scores.put("Three", 0);
+        scores.put("Fours", 0);
+        scores.put("Fives", 0);
+        scores.put("Sixes", 0);
+        scores.put("FullHouse", 0);
+        scores.put("FourOfAKind", 0);
+        scores.put("Straight", 0);
+        scores.put("YAHTZEE", 0);
+    }
+
+    public void addScore(String type, int value) throws Exception {
+        if (checkValid(type, value)) {
+            scores.replace(type, value);
+        }
+        else{
+            /*
+            create an exception
+             */
+            throw new Exception();
+        }
+    }
+
+    public boolean checkValid(String type, int value){
+        return player.getDices().getScoreOptions().get(type) == value;
+    }
+
 }
