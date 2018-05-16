911ba2caeb7adfe9bc976967b0b1035a97ae6772
gihpuccini <gxp6079@g.rit.edu>
2018-05-16 18:29:09 -0400

    In progress

--- src/Controle/Player.java
+++ src/Controle/Player.java
@@ -6,12 +6,10 @@ public class Player {
 
     private String name;
     private DiceTable dices;
-    private ScoreTable scoreTable;
 
     public Player(String name){
         this.name = name;
         this.dices = new DiceTable();
-        this.scoreTable = new ScoreTable(this);
     }
 
     public DiceTable getDices() {
@@ -20,5 +18,9 @@ public class Player {
 
     public Pair chooseScore(String scoreType, int value){
         return new Pair(scoreType, value);
+    }
+
+    public String getName() {
+        return name;
     }
 }
--- src/Controle/ScoreTable.java
+++ src/Controle/ScoreTable.java
@@ -4,38 +4,65 @@ import java.util.HashMap;
 
 public class ScoreTable {
 
-    Player player;
-    HashMap<String, Integer> scores;
+    Player player1;
+    Player player2;
+    HashMap<String, Integer> scoresPlayer1;
+    HashMap<String, Integer> scoresPlayer2;
 
-    public ScoreTable(Player player){
-        this.player = player;
-        this.scores = new HashMap<>();
-        scores.put("Ones", 0);
-        scores.put("Two", 0);
-        scores.put("Three", 0);
-        scores.put("Fours", 0);
-        scores.put("Fives", 0);
-        scores.put("Sixes", 0);
-        scores.put("FullHouse", 0);
-        scores.put("FourOfAKind", 0);
-        scores.put("Straight", 0);
-        scores.put("YAHTZEE", 0);
+    public ScoreTable(Player player1, Player player2){
+        this.player1 = player1;
+        this.player2 = player2;
+        this.scoresPlayer1 = new HashMap<>();
+        this.scoresPlayer2 = new HashMap<>();
+        scoresPlayer1.put("Ones", 0);
+        scoresPlayer1.put("Two", 0);
+        scoresPlayer1.put("Three", 0);
+        scoresPlayer1.put("Fours", 0);
+        scoresPlayer1.put("Fives", 0);
+        scoresPlayer1.put("Sixes", 0);
+        scoresPlayer1.put("FullHouse", 0);
+        scoresPlayer1.put("FourOfAKind", 0);
+        scoresPlayer1.put("Straight", 0);
+        scoresPlayer1.put("YAHTZEE", 0);
+        scoresPlayer2.put("Ones", 0);
+        scoresPlayer2.put("Two", 0);
+        scoresPlayer2.put("Three", 0);
+        scoresPlayer2.put("Fours", 0);
+        scoresPlayer2.put("Fives", 0);
+        scoresPlayer2.put("Sixes", 0);
+        scoresPlayer2.put("FullHouse", 0);
+        scoresPlayer2.put("FourOfAKind", 0);
+        scoresPlayer2.put("Straight", 0);
+        scoresPlayer2.put("YAHTZEE", 0);
     }
 
-    public void addScore(String type, int value) throws Exception {
-        if (checkValid(type, value)) {
-            scores.replace(type, value);
+    public void addScore(String player, String type, int value) throws Exception {
+        if (checkValid(player, type, value)) {
+            if (player.equals(player1.getName())){
+                scoresPlayer1.replace(type, value);
+            }
+            else if(player.equals(player2.getName())){
+                scoresPlayer2.replace(type, value);
+            }
         }
         else{
             /*
-            create an exception
+            create invalid score exception exception
              */
             throw new Exception();
         }
     }
 
-    public boolean checkValid(String type, int value){
-        return player.getDices().getScoreOptions().get(type) == value;
+    public boolean checkValid(String player, String type, int value) throws Exception{
+        if(player.equals(player1.getName())) {
+            return player1.getDices().getScoreOptions().get(type) == value;
+        }
+        else if(player.equals(player2.getName())){
+            return player2.getDices().getScoreOptions().get(type) == value;
+        }
+        else {
+            throw new Exception();
+        }
     }
 
 }
